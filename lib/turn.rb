0a81813ac7a74745997e3ee54e9cb9695574961f
Michael Carroll <carrollm2@gmail.com>
2019-03-02 02:29:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,8 @@ def turn(board)
 
   while valid_move?(board, index) == false
     puts "Please enter 1-9:\n"
+    input = gets.strip
+    index = input_to_index(input)
   end
 
   if valid_move?(board, index) == true
