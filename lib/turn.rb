566ff88741a05bbe437e8d74424f999d05781f81
Michael Carroll <carrollm2@gmail.com>
2019-03-02 02:58:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,9 +41,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index == -1
-    index += 1
-  end
+
   if index.between?(0, 8)
     if position_taken?(board, index) == false
       return true
