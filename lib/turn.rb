bbc24a450d23ef00e1a1ea12236a751e6835eeca
Michael Carroll <carrollm2@gmail.com>
2019-03-02 03:30:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,7 +44,6 @@ def valid_move?(board, index)
 
   if index.between?(0, 8)
     if position_taken?(board, index) == false
-      print "should be evaluating to true"
       return true
     else
       return false
